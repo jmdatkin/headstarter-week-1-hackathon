2908e46cb8df02e0c38035d4bbf3be0773a23c66
mafruhamanal <mafruhamanal1@gmail.com>
2024-07-27 08:31:42 -0400

    feat: hero content + rm footer

--- src/_components/Hero.tsx
+++ src/_components/Hero.tsx
@@ -11,7 +11,7 @@ export default function Hero() {
           <Text size="lg" my="md">
             We provide amazing blah to boost your blah.
           </Text>
-          <Button size="lg" variant="filled" color="blue">
+          <Button size="lg" variant="filled" color="blue" className='text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2'>
             Get Started
           </Button>
         </section>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
-import Footer from "@/_components/Footer";
 import Hero from "@/_components/Hero";
-import Navbar from "@/_components/Navbar";
 import { HydrateClient } from "@/trpc/server";
 
 export default async function Home() {
   return (
     <HydrateClient>
       <main>
-        <Navbar/>
         <Hero/>
-        <Footer/>
       </main>
     </HydrateClient>
   );
