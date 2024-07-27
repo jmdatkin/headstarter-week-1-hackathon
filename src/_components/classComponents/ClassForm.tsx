3f9d22342b83cb90ee36dcb1905234708ba1f0ce
mafruhamanal <mafruhamanal1@gmail.com>
2024-07-27 06:13:06 -0400

    feat: classform hooks

--- src/_components/classComponents/ClassForm.tsx
+++ src/_components/classComponents/ClassForm.tsx
@@ -1,22 +1,25 @@
 'use client';
 
-import { SegmentedControl, TextInput } from "@mantine/core";
-import { useState, useRef } from 'react';
+import { TextInput, Textarea, Button } from "@mantine/core";
+import { useForm } from "@mantine/form";
+import { useState } from 'react';
 import classes from './../../styles/FloatingLabelInput.module.css';
 
 export default function ClassForm() {
+    const form = useForm({
+        initialValues: {
+            className: "",
+            classDescription: ""
+        },
+    });
 
-    const [focused, setFocused] = useState(false);
-    const [value, setValue] = useState('');
     const [buttonLoading, setButtonLoading] = useState(false);
-    const floating = value.trim().length !== 0 || focused || undefined;
-    const formRef = useRef<HTMLFormElement>(null);
 
     const handleFinalSubmit = (event: React.FormEvent) => {
         event.preventDefault();
         setButtonLoading(true);
         // Add your form submission logic here
-        console.log('Form submitted');
+        console.log('Form submitted', form.values);
         setTimeout(() => {
             setButtonLoading(false);
         }, 2000); // Simulate an async operation
@@ -24,7 +27,7 @@ export default function ClassForm() {
 
     return (
         <div className="bg-purple-100 min-h-screen flex items-center justify-center">
-            <form ref={formRef} onSubmit={handleFinalSubmit} className="mx-8 my-6">
+            <form onSubmit={handleFinalSubmit} className="mx-8 my-6">
                 <div className="p-24 flex flex-col justify-center items-center">
                     <h2 className="pb-10 font-bold text-6xl text-neutral-600 text-center">Create Class</h2>
                     <TextInput
@@ -33,33 +36,16 @@ export default function ClassForm() {
                         required
                         className="w-[20rem] mb-4"
                         classNames={classes}
-                        value={value}
-                        onChange={(event) => setValue(event.currentTarget.value)}
-                        onFocus={() => setFocused(true)}
-                        onBlur={() => setFocused(false)}
-                        mt="md"
-                        autoComplete="nope"
-                        data-floating={floating}
-                        labelProps={{ 'data-floating': floating }}
+                        {...form.getInputProps('className')}
                     />
-
-                    <div className="flex flex-row gap-4">
-                    <TextInput
+                    <Textarea
                         label="Class Description"
                         placeholder="Class Description"
                         required
                         className="w-[20rem] mb-4"
                         classNames={classes}
-                        value={value}
-                        onChange={(event) => setValue(event.currentTarget.value)}
-                        onFocus={() => setFocused(true)}
-                        onBlur={() => setFocused(false)}
-                        mt="md"
-                        autoComplete="nope"
-                        data-floating={floating}
-                        labelProps={{ 'data-floating': floating }}
+                        {...form.getInputProps('classDescription')}
                     />
-                    </div>
                     <button
                         className='rounded-lg px-4 py-2 bg-blue-500 text-blue-100 hover:bg-blue-600 duration-300'
                         type='submit'
