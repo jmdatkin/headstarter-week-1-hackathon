e04b0d58abad48e8f808abde94271ff284b24fed
mafruhamanal <mafruhamanal1@gmail.com>
2024-07-27 10:09:55 -0400

    hero component to home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import Welcome from "@/_components/Welcome";
+import Hero from "@/_components/Hero";
 import { HydrateClient } from "@/trpc/server";
 
 export default async function Home() {
   return (
     <HydrateClient>
       <main>
-        <Welcome/>
+        <Hero/>
       </main>
     </HydrateClient>
   );
