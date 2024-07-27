084ebad5b0a814e6db153eb8cde0417f203416a5
zaida04 <nico.03727+github@gmail.com>
2024-07-27 04:23:46 -0400

    feat: some more create assignment info

--- src/app/classes/[classId]/material/create/page.tsx
+++ src/app/classes/[classId]/material/create/page.tsx
@@ -1,27 +1,36 @@
-import { SegmentedControl, Text, Title } from "@mantine/core";
+import { SegmentedControl, Text, TextInput, Title } from "@mantine/core";
 
 export default async function CreateMaterialPage() {
     return (
         <form className="mx-8 my-6">
             <h2 className="mb-4">Create material</h2>
 
-            <p>Material Type</p>
-            <SegmentedControl
-                data={[
-                    {
-                        value: "1",
-                        label: "Reading",
-                    },
-                    {
-                        value: "2",
-                        label: "Homework"
-                    },
-                    {
-                        value: "3",
-                        label: "Quiz"
-                    }
-                ]}
-            />
+            <div className="flex flex-row gap-4">
+                <div>
+                    <p>Material Title</p>
+                    <TextInput className="w-[20rem] mb-4" />
+                </div>
+
+                <div>
+                    <p>Material Type</p>
+                    <SegmentedControl
+                        data={[
+                            {
+                                value: "1",
+                                label: "Reading",
+                            },
+                            {
+                                value: "2",
+                                label: "Homework"
+                            },
+                            {
+                                value: "3",
+                                label: "Quiz"
+                            }
+                        ]}
+                    />
+                </div>
+            </div>
         </form>
     );
 }
