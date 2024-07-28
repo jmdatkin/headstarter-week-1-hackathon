01dec470057a3fa90cd4a6f492293506ea73e25d
zaida04 <nico.03727+github@gmail.com>
2024-07-28 09:10:05 -0400

    fix: use libsql

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,6 +1,12 @@
-import { drizzle } from "drizzle-orm/better-sqlite3";
-import Database from "better-sqlite3";
+// import { drizzle } from "drizzle-orm/better-sqlite3";
+import { drizzle } from "drizzle-orm/libsql";
+// import Database from "better-sqlite3";
 import * as schema from "./schema";
+import { createClient } from '@libsql/client';
 
-const conn = new Database("database.db");
-export const db = drizzle(conn, { schema });
+// const conn = new Database("database.db");
+const client = createClient({
+    url: process.env.TURSO_CONNECTION_URL!,
+    authToken: process.env.TURSO_AUTH_TOKEN!,
+});
+export const db = drizzle(client, { schema });
