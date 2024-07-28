655ff0966dd84977fcf8caa6ae69906ceaa24638
zaida04 <nico.03727+github@gmail.com>
2024-07-28 08:51:06 -0400

    feat: move to turso

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,8 +3,10 @@ import { defineConfig } from "drizzle-kit";
 export default defineConfig({
   schema: "./src/server/db/schema.ts",
   dialect: "sqlite",
+  driver: "turso",
   dbCredentials: {
-    url: "./database.db",
+    url: process.env.TURSO_CONNECTION_URL!,
+    authToken: process.env.TURSO_AUTH_TOKEN!,
   },
   verbose: true,
   strict: true,
